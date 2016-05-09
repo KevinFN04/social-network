364db4fd83fcea7299cc1376a0ced23eb1f04f7f
KevinFN04 <kevin.felix.04@gmail.com>
2016-05-09 12:08:13 +0200

    Creadas las clases segun UML

---
+++ PhotoPost.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/**
+ * Write a description of class PhotoPost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PhotoPost
+{
+    private String username;
+    private String message;
+    private String caption;
+    private long timestamp;
+    private int likes;
+    private ArrayList<String> comments;
+    
+    /**
+     * Constructor for objects of class PhotoPost
+     */
+    public PhotoPost()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        return 1;
+    }
+}
