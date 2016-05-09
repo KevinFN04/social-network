fe86c61ada7ae0340eb75a4754365113edae395d
KevinFN04 <kevin.felix.04@gmail.com>
2016-05-09 13:27:53 +0200

    Creada la clase NewsFeed

--- NewsFeed.java
+++ NewsFeed.java
@@ -20,16 +20,22 @@ public class NewsFeed
 
     public void addMessagePost(MessagePost message)
     {
-
+        messages.add(message);
     }
 
     public void addPhotoPost(PhotoPost photo)
     {
-
+        photos.add(photo);
     }
 
     public void show()
     {
-
+        for(MessagePost message: messages){
+            message.display();
+        }
+        System.out.println();
+        for(PhotoPost photo: photos){
+            photo.display();
+        }
     }
 }
