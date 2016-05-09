f85b12ca81011a6e09c275ac957f15fd8453ead7
KevinFN04 <kevin.felix.04@gmail.com>
2016-05-09 13:14:26 +0200

    Creada la clase MessagePost

--- MessagePost.java
+++ MessagePost.java
@@ -20,24 +20,29 @@ public class MessagePost
     {
         username = author;
         message = text;
-        timestamp = 0;
+        timestamp = System.currentTimeMillis();
         likes = 0;
         comments = new ArrayList<>();
     }
 
     public void like()
     {
-
+        likes++;
     }
 
     public void unLike()
     {
-
+        if(likes <= 0){
+            likes = 0;
+        }
+        else{
+            likes--;
+        }
     }
 
     public void addComment(String text)
     {
-
+        comments.add(text);
     }
 
     public String getText()
@@ -46,18 +51,35 @@ public class MessagePost
     }
 
     public long getTimeStamp()
-    {
+    {   
         return timestamp;
     }
 
     public void display()
     {
-
+        System.out.println("Username: " + username);
+        System.out.println("Message: " + message);
+        System.out.println("Submitted: " + timeString(System.currentTimeMillis()));
+        System.out.println("Likes: " + likes);
+        if(comments.size() <= 0){
+            System.out.println("No comments");
+        }
+        else{
+            System.out.println("Comments: ");
+            int count = 1;
+            for(String comment: comments){
+                System.out.println(count + ": " + comment);
+                count++;
+            }
+        }
     }
 
     public String timeString(long time)
     {
-        String timeString = "";
+        long tiempo = (time - timestamp)/1000;
+        long minutes = (tiempo /60);
+        long seconds = tiempo - (minutes * 60);        
+        String timeString = minutes + " Mins " + seconds + " Secs.";
         return timeString;
     }
 }
