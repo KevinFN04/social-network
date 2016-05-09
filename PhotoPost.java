562ebb2460bb3b561f129c2d74b16ceb6d9a5f38
KevinFN04 <kevin.felix.04@gmail.com>
2016-05-09 13:20:57 +0200

    Creada la clase PhotoPost

--- PhotoPost.java
+++ PhotoPost.java
@@ -8,28 +8,87 @@ import java.util.*;
 public class PhotoPost
 {
     private String username;
-    private String message;
+    private String filename;
     private String caption;
     private long timestamp;
     private int likes;
     private ArrayList<String> comments;
-    
+
     /**
      * Constructor for objects of class PhotoPost
      */
-    public PhotoPost()
+    public PhotoPost(String author, String fileName, String caption)
     {
-        
+        this.filename = filename;
+        this.caption = caption;
+        username = author;
+        timestamp = System.currentTimeMillis();
+        likes = 0;
+        comments = new ArrayList<>();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    public void like()
     {
-        return 1;
+        likes++;
+    }
+
+    public void unLike()
+    {
+        if(likes <= 0){
+            likes = 0;
+        }
+        else{
+            likes--;
+        }
+    }
+
+    public void addComment(String text)
+    {
+        comments.add(text);
+    }
+
+    public String getImageFile()
+    {
+        String imageFile = filename;
+        return imageFile;
+    }
+
+    public String getCaption()
+    {
+        return caption;
+    }
+
+    public long getTimeStamp()
+    {
+        return timestamp;
+    }
+
+    public void display()
+    {
+        System.out.println("Username: " + username);
+        System.out.println("Photo: " + filename);
+        System.out.println("Caption: " + caption);
+        System.out.println("Submitted: " + timeString(System.currentTimeMillis()));
+        System.out.println("Likes: " + likes);
+        if(comments.size() <= 0){
+            System.out.println("No comments");
+        }
+        else{
+            System.out.println("Comments: ");
+            int count = 1;
+            for(String comment: comments){
+                System.out.println(count + ": " + comment);
+                count++;
+            }
+        }
+    }
+
+    public String timeString(long time)
+    {
+        long tiempo = (time - timestamp)/1000;
+        long minutes = (tiempo /60);
+        long seconds = tiempo - (minutes * 60);        
+        String timeString = minutes + " Mins " + seconds + " Secs.";
+        return timeString;
     }
 }
